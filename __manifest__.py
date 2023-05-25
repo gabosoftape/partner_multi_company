148b098570bd38bda09ca9e993c7893c16d5a2e3
Gabriel Pabon <gabosoft.ape@gmail.com>
2023-05-25 11:12:18 -0500

    update base partner multicompany

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,18 @@
-# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Oihane Crucelaegui
+# Copyright 2015-2019 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Partner multi-company",
-    "summary": "Select individually partner visibility on each " "company",
-    "author": "Gabosoft," "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/multi-company",
-    "category": "Product Management",
-    "version": "15.0.2.0.1",
+    "summary": "Select individually the partner visibility on each company",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["base_multi_company", "contacts"],
-    "data": [
-        "views/res_partner_views.xml",
-        "security/partner_multi_company_security.xml",
-    ],
+    "depends": ["base_multi_company"],
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "category": "Partner Management",
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
 }
